b2dec1552cac4dbd156bdc2d9d2ce19305f1f9c6
EazyDean <71340598+EazyDean@users.noreply.github.com>
2021-12-10 09:25:55 -0700

    update

--- BlackHoleMetaphor/BlackHoleMetaphor.pde
+++ BlackHoleMetaphor/BlackHoleMetaphor.pde
@@ -1,12 +1,18 @@
 //Global Variables et al
+int StarNumbers = 25;
+Star[] stars = new Star[StarNumbers]
 
 void setup() {
 fullScreen();
-
+ for (int i=0; i<stars.length; i++) {
+    stars[i] = new Star(random(width), random(height), random(1, 10));}
 }//End setup()
 
 
-void draw() {}//End draw()
+void draw() {
+for (int i=0; i<stars.length; i++) {
+      ellipse(stars[i].x, stars[i].y, stars[i].diameter, stars[i].diameter);}
+}//End draw()
 
 void mousePressed() {}//End mousePressed
 
--- BlackHoleMetaphor/Star.java
+++ BlackHoleMetaphor/Star.java
@@ -1,13 +1,16 @@
 class Star {
   //Global Variables, encapsulated
+  float x, y, diameter;
   //
-  //Variable Population
-  Star() {
+  // Variable Population
+  Star(float xParameter, float yParameter, float diameterParameter) {
+    this.x = xParameter;
+    this.y = yParameter;
+    this.diameter = diameterParameter;
   }//End Constructor
   //
   void draw() {}//End draw()
   //
-  void step() {}//End step()
-  //
-  //Getters and Setters
-  //
+  void step() {
+  }//End step()
+}//End Star
